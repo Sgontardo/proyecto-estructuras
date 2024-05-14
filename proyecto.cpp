ffb61df38ee59c3d321d50444714f4af3d37832b
Sergio <sergiogontardo@gmail.com>
2024-05-13 22:35:08 -0400

    update de archivo proyecto

---
+++ proyecto.cpp
@@ -0,0 +1,78 @@
+/*
+Primera parte proyecto Estructura de Datos
+Integrantes:
+    Sergio Gontardo
+    Leonardo Chacón
+    Daniel Vasquez
+*/
+
+/*
+Estructuras que tenemos que añadir:
+    Raza:
+        Nombre
+        Energía
+        Salud: 100%
+        Ambiente
+        Fidelidad
+    Accesorios:
+        Nombre
+        Tipo (ataque, defensa, supervivencia y adaptación)
+        Valor
+        Energía
+        Contenedor
+    Ambiente (Elección aleatoria):
+        Nombre
+*/
+
+/*
+Creación de jugadores con su mochila
+    Soldado (Nombre del player, elegir raza)
+        Salud: 100%
+        Energía: Según la raza
+    Mochila (Elegir 5 accesorios para mochila)
+        Accesorios
+    Equipo (Aleatorio)
+*/
+
+/*
+Manejo de Menú
+    1. Acciones para raza
+        1.1 Crear raza
+        1.2 Consultar raza
+        1.3 Modificar raza
+        1.4 Eliminar raza
+    2. Crear accesorio
+        2.2 Crear accesorio
+        2.3 Consultar accesorio
+        2.4 Modificar accesorio
+        2.5 Eliminar accesorio
+    3. Agregar ambiente
+        3.3 Crear ambiente
+        3.4 Consultar ambiente
+        3.5 Modificar ambiente
+        3.6 Eliminar ambiente
+    4. Crear jugador
+        4.1 Soldado
+            4.1.1 Asignar raza
+            4.1.2 Agregar accesorios a mochila
+            4.1.3 Asignar ambiente
+    5. Salir
+*/
+
+/*
+Uso de lista enlazada
+    Crear
+    Leer
+    Modificar
+    Eliminar
+*/
+
+#include <iostream>;
+
+using namespace std;
+
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
