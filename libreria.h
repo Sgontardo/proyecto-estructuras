6c444817dd19813833eb1770436c168b3c47384a
Sergio <sergiogontardo@gmail.com>
2024-05-17 21:02:31 -0400

    modificación de archivo y creación de libreria con estructuras

--- libreria.h
+++ libreria.h
@@ -2,127 +2,332 @@
 
 using namespace std;
 
-struct nodo {
-    int dato;
-    nodo *siguiente;
-} *primero, *ultimo;
+struct raza {
+    string nombre;
+    int energia;
+    int salud;
+    string ambiente;
+    raza *siguiente;
+} *primeroRaza, *ultimoRaza;
 
+struct accesorio {
+    string nombre;
+    string tipo;
+    int valor;
+    int energia;
+    string contenedor;
+    accesorio *siguiente;
+} *primeroAccesorio, *ultimoAccesorio;
 
-void crearNodo();
+struct ambiente {
+    string nombre;
+    ambiente *siguiente;
+} *primeroAmbiente, *ultimoAmbiente;
 
-void crearNodo() {
-    nodo *nuevo = new nodo();
-    cout << "Ingrese el dato: ";
-    cin >> nuevo -> dato;
+void crearRaza() {
+    raza *nuevo = new raza();
+    cout << "Ingrese el nombre de la raza: ";
+    cin >> nuevo -> nombre;
+    cout << "Ingrese la energía de la raza: ";
+    cin >> nuevo -> energia;
+    nuevo -> salud = 100;
+    cout << "Ingrese el ambiente de la raza: ";
+    cin >> nuevo -> ambiente;
 
-    if (primero == NULL) {
-        primero = nuevo;
-        primero -> siguiente = NULL;
-        ultimo = nuevo;
+    if (primeroRaza == NULL) {
+        primeroRaza = nuevo;
+        primeroRaza -> siguiente = NULL;
+        ultimoRaza = nuevo;
     } else {
-        ultimo -> siguiente = nuevo;
+        ultimoRaza -> siguiente = nuevo;
         nuevo -> siguiente = NULL;
-        ultimo = nuevo;
+        ultimoRaza = nuevo;
     }
 
-    cout << "Nodo ingresado" << endl;
+    cout << "Raza ingresada" << endl;
 }
 
-
-void leerNodo();
-
-void leerNodo() {
-    nodo *actual = new nodo();
-    actual = primero;
-    if (primero != NULL) {
+void leerRaza() {
+    raza *actual = new raza();
+    actual = primeroRaza;
+    if (primeroRaza != NULL) {
         while (actual != NULL) {
-            cout << endl << " " << actual -> dato;
+            cout << endl << "Nombre: " << actual -> nombre << endl;
+            cout << "Energía: " << actual -> energia << endl;
+            cout << "Salud: " << actual -> salud << endl;
+            cout << "Ambiente: " << actual -> ambiente << endl;
             actual = actual -> siguiente;
         }
     } else {
-        cout << "La lista se encuentra vacía" << endl;
+        cout << "La lista de razas se encuentra vacía" << endl;
     }
 }
 
-
-void modificarNodo();
-
-void modificarNodo() {
-    nodo *actual = new nodo();
-    actual = primero;
+void modificarRaza() {
+    raza *actual = new raza();
+    actual = primeroRaza;
     bool encontrado = false;
-    int nodoBuscado = 0;
-    cout << "Ingrese el dato del nodo a modificar: ";
-    cin >> nodoBuscado;
-    if (primero != NULL) {
+    string razaBuscada;
+    cout << "Ingrese el nombre de la raza a modificar: ";
+    cin >> razaBuscada;
+    if (primeroRaza != NULL) {
         while (actual != NULL && encontrado != true) {
-            if (actual -> dato == nodoBuscado) {
-                cout << "Nodo con el dato " << nodoBuscado << " encontrado" << endl;
-                cout << "Ingrese el nuevo dato: ";
-                cin >> actual -> dato;
-                cout << "Nodo modificado" << endl;
+            if (actual -> nombre == razaBuscada) {
+                cout << "Raza con el nombre " << razaBuscada << " encontrada" << endl;
+                cout << "Ingrese el nuevo nombre: ";
+                cin >> actual -> nombre;
+                cout << "Ingrese la nueva energía: ";
+                cin >> actual -> energia;
+                cout << "Ingrese el nuevo ambiente: ";
+                cin >> actual -> ambiente;
+                cout << "Raza modificada" << endl;
                 encontrado = true;
             }
             actual = actual -> siguiente;
         }
         if (!encontrado) {
-            cout << "Nodo no encontrado" << endl;
+            cout << "Raza no encontrada" << endl;
         }
     } else {
-        cout << "La lista se encuentra vacía" << endl;
+        cout << "La lista de razas se encuentra vacía" << endl;
     }
 }
 
-
-void borrarNodo();
-
-void borrarNodo() {
-    nodo *actual = new nodo();
-    actual = primero;
-    nodo *anterior = new nodo();
+void eliminarRaza() {
+    raza *actual = new raza();
+    actual = primeroRaza;
+    raza *anterior = new raza();
     anterior = NULL;
     bool encontrado = false;
-    int nodoBuscado = 0;
-    cout << "Ingrese el dato del nodo a borrar: ";
-    cin >> nodoBuscado;
-    if (primero != NULL) {
+    string razaBuscada;
+    cout << "Ingrese el nombre de la raza a eliminar: ";
+    cin >> razaBuscada;
+    if (primeroRaza != NULL) {
         while (actual != NULL && encontrado != true) {
-            if (actual -> dato == nodoBuscado) {
-                cout << "Nodo con el dato " << nodoBuscado << " encontrado" << endl;
-                if (actual == primero) {
-                    primero = primero -> siguiente;
-                } else if (actual == ultimo) {
+            if (actual -> nombre == razaBuscada) {
+                cout << "Raza con el nombre " << razaBuscada << " encontrada" << endl;
+                if (actual == primeroRaza) {
+                    primeroRaza = primeroRaza -> siguiente;
+                } else if (actual == ultimoRaza) {
                     anterior -> siguiente = NULL;
-                    ultimo = anterior;
+                    ultimoRaza = anterior;
                 } else {
                     anterior -> siguiente = actual -> siguiente;
                 }
-                cout << "Nodo eliminado" << endl;
+                cout << "Raza eliminada" << endl;
                 encontrado = true;
             }
             anterior = actual;
             actual = actual -> siguiente;
         }
         if (!encontrado) {
-            cout << "Nodo no encontrado" << endl;
+            cout << "Raza no encontrada" << endl;
         }
     } else {
-        cout << "La lista se encuentra vacía" << endl;
+        cout << "La lista de razas se encuentra vacía" << endl;
     }
 }
 
+void crearAccesorio() {
+    accesorio *nuevo = new accesorio();
+    cout << "Ingrese el nombre del accesorio: ";
+    cin >> nuevo -> nombre;
+    cout << "Ingrese el tipo del accesorio: ";
+    cin >> nuevo -> tipo;
+    cout << "Ingrese el valor del accesorio: ";
+    cin >> nuevo -> valor;
+    cout << "Ingrese la energía del accesorio: ";
+    cin >> nuevo -> energia;
+    cout << "Ingrese el contenedor del accesorio: ";
+    cin >> nuevo -> contenedor;
 
-void recorrerLista();
+    if (primeroAccesorio == NULL) {
+        primeroAccesorio = nuevo;
+        primeroAccesorio -> siguiente = NULL;
+        ultimoAccesorio = nuevo;
+    } else {
+        ultimoAccesorio -> siguiente = nuevo;
+        nuevo -> siguiente = NULL;
+        ultimoAccesorio = nuevo;
+    }
 
-void recorrerLista() {
-    nodo *actual = new nodo();
-    actual = primero;
-    if (primero != NULL) {
+    cout << "Accesorio ingresado" << endl;
+}
+
+void leerAccesorio() {
+    accesorio *actual = new accesorio();
+    actual = primeroAccesorio;
+    if (primeroAccesorio != NULL) {
         while (actual != NULL) {
-            cout << " " << actual -> dato;
+            cout << endl << "Nombre: " << actual -> nombre << endl;
+            cout << "Tipo: " << actual -> tipo << endl;
+            cout << "Valor: " << actual -> valor << endl;
+            cout << "Energía: " << actual -> energia << endl;
+            cout << "Contenedor: " << actual -> contenedor << endl;
             actual = actual -> siguiente;
         }
     } else {
-        cout << "La lista se encuentra vacía" << endl;
+        cout << "La lista de accesorios se encuentra vacía" << endl;
+    }
+}
+
+void modificarAccesorio() {
+    accesorio *actual = new accesorio();
+    actual = primeroAccesorio;
+    bool encontrado = false;
+    string accesorioBuscado;
+    cout << "Ingrese el nombre del accesorio a modificar: ";
+    cin >> accesorioBuscado;
+    if (primeroAccesorio != NULL) {
+        while (actual != NULL && encontrado != true) {
+            if (actual -> nombre == accesorioBuscado) {
+                cout << "Accesorio con el nombre " << accesorioBuscado << " encontrado" << endl;
+                cout << "Ingrese el nuevo nombre: ";
+                cin >> actual -> nombre;
+                cout << "Ingrese el nuevo tipo: ";
+                cin >> actual -> tipo;
+                cout << "Ingrese el nuevo valor: ";
+                cin >> actual -> valor;
+                cout << "Ingrese la nueva energía: ";
+                cin >> actual -> energia;
+                cout << "Ingrese el nuevo contenedor: ";
+                cin >> actual -> contenedor;
+                cout << "Accesorio modificado" << endl;
+                encontrado = true;
+            }
+            actual = actual -> siguiente;
+        }
+        if (!encontrado) {
+            cout << "Accesorio no encontrado" << endl;
+        }
+    } else {
+        cout << "La lista de accesorios se encuentra vacía" << endl;
+    }
+}
+
+void eliminarAccesorio() {
+    accesorio *actual = new accesorio();
+    actual = primeroAccesorio;
+    accesorio *anterior = new accesorio();
+    anterior = NULL;
+    bool encontrado = false;
+    string accesorioBuscado;
+    cout << "Ingrese el nombre del accesorio a eliminar: ";
+    cin >> accesorioBuscado;
+    if (primeroAccesorio != NULL) {
+        while (actual != NULL && encontrado != true) {
+            if (actual -> nombre == accesorioBuscado) {
+                cout << "Accesorio con el nombre " << accesorioBuscado << " encontrado" << endl;
+                if (actual == primeroAccesorio) {
+                    primeroAccesorio = primeroAccesorio -> siguiente;
+                } else if (actual == ultimoAccesorio) {
+                    anterior -> siguiente = NULL;
+                    ultimoAccesorio = anterior;
+                } else {
+                    anterior -> siguiente = actual -> siguiente;
+                }
+                cout << "Accesorio eliminado" << endl;
+                encontrado = true;
+            }
+            anterior = actual;
+            actual = actual -> siguiente;
+        }
+        if (!encontrado) {
+            cout << "Accesorio no encontrado" << endl;
+        }
+    } else {
+        cout << "La lista de accesorios se encuentra vacía" << endl;
+    }
+}
+
+void crearAmbiente() {
+    ambiente *nuevo = new ambiente();
+    cout << "Ingrese el nombre del ambiente: ";
+    cin >> nuevo -> nombre;
+
+    if (primeroAmbiente == NULL) {
+        primeroAmbiente = nuevo;
+        primeroAmbiente -> siguiente = NULL;
+        ultimoAmbiente = nuevo;
+    } else {
+        ultimoAmbiente -> siguiente = nuevo;
+        nuevo -> siguiente = NULL;
+        ultimoAmbiente = nuevo;
+    }
+
+    cout << "Ambiente ingresado" << endl;
+}
+
+void leerAmbiente() {
+    ambiente *actual = new ambiente();
+    actual = primeroAmbiente;
+    if (primeroAmbiente != NULL) {
+        while (actual != NULL) {
+            cout << endl << "Nombre: " << actual -> nombre << endl;
+            actual = actual -> siguiente;
+        }
+    } else {
+        cout << "La lista de ambientes se encuentra vacía" << endl;
+    }
+}
+
+void modificarAmbiente() {
+    ambiente *actual = new ambiente();
+    actual = primeroAmbiente;
+    bool encontrado = false;
+    string ambienteBuscado;
+    cout << "Ingrese el nombre del ambiente a modificar: ";
+    cin >> ambienteBuscado;
+    if (primeroAmbiente != NULL) {
+        while (actual != NULL && encontrado != true) {
+            if (actual -> nombre == ambienteBuscado) {
+                cout << "Ambiente con el nombre " << ambienteBuscado << " encontrado" << endl;
+                cout << "Ingrese el nuevo nombre: ";
+                cin >> actual -> nombre;
+                cout << "Ambiente modificado" << endl;
+                encontrado = true;
+            }
+            actual = actual -> siguiente;
+        }
+        if (!encontrado) {
+            cout << "Ambiente no encontrado" << endl;
+        }
+    } else {
+        cout << "La lista de ambientes se encuentra vacía" << endl;
+    }
+}
+
+void eliminarAmbiente() {
+    ambiente *actual = new ambiente();
+    actual = primeroAmbiente;
+    ambiente *anterior = new ambiente();
+    anterior = NULL;
+    bool encontrado = false;
+    string ambienteBuscado;
+    cout << "Ingrese el nombre del ambiente a eliminar: ";
+    cin >> ambienteBuscado;
+    if (primeroAmbiente != NULL) {
+        while (actual != NULL && encontrado != true) {
+            if (actual -> nombre == ambienteBuscado) {
+                cout << "Ambiente con el nombre " << ambienteBuscado << " encontrado" << endl;
+                if (actual == primeroAmbiente) {
+                    primeroAmbiente = primeroAmbiente -> siguiente;
+                } else if (actual == ultimoAmbiente) {
+                    anterior -> siguiente = NULL;
+                    ultimoAmbiente = anterior;
+                } else {
+                    anterior -> siguiente = actual -> siguiente;
+                }
+                cout << "Ambiente eliminado" << endl;
+                encontrado = true;
+            }
+            anterior = actual;
+            actual = actual -> siguiente;
+        }
+        if (!encontrado) {
+            cout << "Ambiente no encontrado" << endl;
+        }
+    } else {
+        cout << "La lista de ambientes se encuentra vacía" << endl;
     }
 }
--- proyecto.cpp
+++ proyecto.cpp
@@ -71,285 +71,67 @@ Uso de lista enlazada
 #include <string>
 #include <fstream>
 
-
 using namespace std;
 
-struct nodo {
-    int dato;
-    nodo *siguiente;
-} *primero, *ultimo;
-
-void crearNodo(int dato) {
-    nodo *nuevo = new nodo;
-    nuevo->dato = dato;
-    nuevo->siguiente = nullptr;
-
-    if (primero == nullptr) {
-        primero = nuevo;
-        ultimo = nuevo;
-    } else {
-        ultimo->siguiente = nuevo;
-        ultimo = nuevo;
-    }
-
-    cout << "Nodo ingresado" << endl;
-}
-
-void leerNodo() {
-    nodo *actual = primero;
-    if (actual!= nullptr) {
-        while (actual!= nullptr) {
-            cout << " " << actual->dato;
-            actual = actual->siguiente;
-        }
-        cout << endl;
-    } else {
-        cout << "La lista se encuentra vacía" << endl;
-    }
-}
-
-void modificarNodo(int datoBuscado, int datoNuevo) {
-    nodo *actual = primero;
-    bool encontrado = false;
-    while (actual!= nullptr &&!encontrado) {
-        if (actual->dato == datoBuscado) {
-            actual->dato = datoNuevo;
-            cout << "Nodo modificado" << endl;
-            encontrado = true;
-        }
-        actual = actual->siguiente;
-    }
-    if (!encontrado) {
-        cout << "Nodo no encontrado" << endl;
-    }
-}
-
-void borrarNodo(int datoBuscado) {
-    nodo *actual = primero, *anterior = nullptr;
-    bool encontrado = false;
-    while (actual!= nullptr &&!encontrado) {
-        if (actual->dato == datoBuscado) {
-            if (anterior == nullptr) {
-                primero = actual->siguiente;
-            } else {
-                anterior->siguiente = actual->siguiente;
-            }
-            if (actual == ultimo) {
-                ultimo = anterior;
-            }
-            delete actual;
-            encontrado = true;
-        }
-        anterior = actual;
-        actual = actual->siguiente;
-    }
-    if (!encontrado) {
-        cout << "Nodo no encontrado" << endl;
-    }
-}
-
-void escribirDatosEnArchivo(nodo* primerNodo) {
-    ofstream archivo("datos.txt");
-    if (archivo.is_open()) {
-        nodo* actual = primerNodo;
-        while (actual!= nullptr) {
-            archivo << actual->dato << "\n";
-            actual = actual->siguiente;
-        }
-        archivo.close();
-    } else {
-        cout << "No se pudo abrir el archivo para escritura." << endl;
-    }
-}
-
-void leerDatosDesdeArchivo() {
-    ifstream archivo("datos.txt");
-    if (archivo.is_open()) {
-        string linea;
-        while (getline(archivo, linea)) {
-            int dato = stoi(linea);
-            crearNodo(dato);
-        }
-        archivo.close();
-    } else {
-        cout << "No se pudo abrir el archivo para lectura." << endl;
-    }
-}
 
 void menu() {
     int opcion;
-    char seguirMenu = 's';
     do {
-        cout << "Manejo de Menú" << endl;
+        cout << endl << "Menú" << endl;
         cout << "1. Acciones para raza" << endl;
         cout << "2. Acciones para accesorio" << endl;
         cout << "3. Acciones para ambiente" << endl;
-        cout << "4. Crear soldado" << endl;
-        cout << "5. Salir" << endl;
+        cout << "4. Salir" << endl;
         cout << "Ingrese una opción: ";
         cin >> opcion;
-
         switch (opcion) {
             case 1:
-                int opcionraza;
+                int opcionRaza;
                 do {
-                    cout << "1 Crear raza" << endl;
-                    cout << "2 Consultar raza" << endl;
-                    cout << "3 Modificar raza" << endl;
-                    cout << "4 Eliminar raza" << endl;
-                    cout << "5 Regresar al menú principal" << endl;
-                    cin >> opcionraza;
-                    switch(opcionraza) {
-                        case 1:
-                            int dato;
-                            cout << "Ingrese el dato del nodo: ";
-                            cin >> dato;
-                            crearNodo(dato);
-                            break;
-                        case 2:
-                            leerNodo();
-                            break;
-                        case 3:
-                            int datoBuscado, datoNuevo;
-                            cout << "Ingrese el dato del nodo a modificar: ";
-                            cin >> datoBuscado;
-                            cout << "Ingrese el nuevo dato del nodo: ";
-                            cin >> datoNuevo;
-                            modificarNodo(datoBuscado, datoNuevo);
-                            break;
-                        case 4:
-                            int datoBorrar;
-                            cout << "Ingrese el dato del nodo a borrar: ";
-                            cin >> datoBorrar;
-                            borrarNodo(datoBorrar);
-                            break;
-                        case 5:
-                            cout << "¿Desea regresar al menú principal? (s/n): ";
-                            cin >> seguirMenu;
-                            if (seguirMenu == 'n') {
-                                break;
-                            } else {
-                                cout << "Regresando al menú principal" << endl;
-                            }
-                            break;
-                        default:
-                            cout << "Opción inválida. Por favor, ingrese una opción válida." << endl;
-                            break;
-                    }
-                } while (opcionraza != 5);
+                    cout << "1. Crear raza" << endl;
+                    cout << "2. Leer razas" << endl;
+                    cout << "3. Modificar razas" << endl;
+                    cout << "4. Eliminar raza" << endl;
+                    cout << "5. Salir" << endl;
+                    cout << "Ingrese una opción: ";
+                    cin >> opcionRaza;
+} while(opcionRaza != 5);
                 break;
             case 2:
-                int opcionaccesorio;
+                int opcionAccesorio;
                 do {
-                    cout << "1 Crear accesorio" << endl;
-                    cout << "2 Consultar accesorio" << endl;
-                    cout << "3 Modificar accesorio" << endl;
-                    cout << "4 Eliminar accesorio" << endl;
-                    cout << "5 Regresar al menú principal" << endl;
-                    cin >> opcionaccesorio;
-                    switch(opcionaccesorio) {
-                        case 1:
-                            int dato;
-                            cout << "Ingrese el dato del nodo: ";
-                            cin >> dato;
-                            crearNodo(dato);
-                            break;
-                        case 2:
-                            leerNodo();
-                            break;
-                        case 3:
-                            int datoBuscado, datoNuevo;
-                            cout << "Ingrese el dato del nodo a modificar: ";
-                            cin >> datoBuscado;
-                            cout << "Ingrese el nuevo dato del nodo: ";
-                            cin >> datoNuevo;
-                            modificarNodo(datoBuscado, datoNuevo);
-                            break;
-                        case 4:
-                            int datoBorrar;
-                            cout << "Ingrese el dato del nodo a borrar: ";
-                            cin >> datoBorrar;
-                            borrarNodo(datoBorrar);
-                            break;
-                        case 5:
-                            cout << "¿Desea regresar al menú principal? (s/n): ";
-                            cin >> seguirMenu;
-                            if (seguirMenu == 'n') {
-                                break;
-                            } else {
-                                cout << "Regresando al menú principal" << endl;
-                            }
-                        default:
-                            cout << "Opción inválida. Por favor, ingrese una opción válida." << endl;
-                            break;
-                    }
-                } while (opcionaccesorio != 5);
-            case 3:
-                int opcionambiente;
-                do {
-                    cout << "1 Crear ambiente" << endl;
-                    cout << "2 Consultar ambiente" << endl;
-                    cout << "3 Modificar ambiente" << endl;
-                    cout << "4 Eliminar ambiente" << endl;
-                    cout << "5 Regresar al menú principal" << endl;
-                    cin >> opcionambiente;
-                    switch(opcionambiente) {
-                        case 1:
-                            int dato;
-                            cout << "Ingrese el dato del nodo: ";
-                            cin >> dato;
-                            crearNodo(dato);
-                            break;
-                        case 2:
-                            leerNodo();
-                            break;
-                        case 3:
-                            int datoBuscado, datoNuevo;
-                            cout << "Ingrese el dato del nodo a modificar: ";
-                            cin >> datoBuscado;
-                            cout << "Ingrese el nuevo dato del nodo: ";
-                            cin >> datoNuevo;
-                            modificarNodo(datoBuscado, datoNuevo);
-                            break;
-                        case 4:
-                            int datoBorrar;
-                            cout << "Ingrese el dato del nodo a borrar: ";
-                            cin >> datoBorrar;
-                            borrarNodo(datoBorrar);
-                            break;
-                        case 5:
-                            cout << "¿Desea regresar al menú principal? (s/n): ";
-                            cin >> seguirMenu;
-                            if (seguirMenu == 'n') {
-                                break;
-                            } else {
-                                cout << "Regresando al menú principal" << endl;
-                            }
-                        default:
-                            cout << "Opción inválida. Por favor, ingrese una opción válida." << endl;
-                            break;
-                    }
-                } while (opcionambiente != 5);
-            case 4:
-                // Crear soldado
-                cout << "Soldado" << endl;
-                cout << "1 Asignar raza" << endl;
-                cout << "2 Agregar accesorios a mochila (5 accesorios)" /*desplegar los accesorios disponibles y añadir con número*/ << endl;
-                cout << "3 Asignar ambiente base" << endl;
+                    cout << "1. Crear accesorio" << endl;
+                    cout << "2. Leer accesorios" << endl;
+                    cout << "3. Modificar accesorios" << endl;
+                    cout << "4. Eliminar accesorio" << endl;
+                    cout << "5. Salir" << endl;
+                    cout << "Ingrese una opción: ";
+                    cin >> opcionAccesorio;
+                } while(opcionAccesorio != 5);
                 break;
-            case 5:
-                // Salir
-                cout << "Salir" << endl;
+            case 3:
+                int opcionAmbiente;
+                do {
+                    cout << "1. Crear ambiente" << endl;
+                    cout << "2. Leer ambientes" << endl;
+                    cout << "3. Modificar ambientes" << endl;
+                    cout << "4. Eliminar ambiente" << endl;
+                    cout << "5. Salir" << endl;
+                    cout << "Ingrese una opción: ";
+                    cin >> opcionAmbiente;
+                } while(opcionAmbiente != 5);
+                break;
+            case 4:
+                cout << "Saliendo" << endl;
                 break;
             default:
-                cout << "Opción inválida. Por favor, ingrese una opción válida." << endl;
+                cout << "Opción no válida" << endl;
                 break;
         }
-    } while (seguirMenu == 's');
+    } while(opcion != 4);
 }
 
 int main() {
-    leerDatosDesdeArchivo();
     menu();
     return 0;
 }
